5ac1b97a0289b979edb4816320700da19cdaa266
yutongucmo <yutongzhao@ucmo.edu>
2024-02-23 19:22:09 -0600

    Create Author domain

---
+++ src/main/java/com/example/demo/domain/Author.java
@@ -0,0 +1,6 @@
+package com.example.demo.domain;
+
+public class Author {
+    // Initially, the file_name "Author" is red --> it means this file has NOT been added to github
+    // Then, let's manually add it
+}
